ccdd75ff6cfe9fa8a8276bf1c8807aae8469e78a
Cian Leypoldt <cianleypoldt@gmail.com>
2025-05-11 11:40:11 +0200

    collition detection

--- src/Physics/Simulation.cpp
+++ src/Physics/Simulation.cpp
@@ -1,1 +1,58 @@
 #include "Simulation.h"
+
+Simulation::Simulation(float time_step, unsigned int iterations, Display *diaplay)
+    : m_display(), m_time_step(time_step), m_iteration_cap(iterations)
+{
+}
+
+void Simulation::removeRigidBody(RigidBody *rb)
+{
+    for (int i = 0; i < m_rigidBodies.size(); i++)
+    {
+        if (m_rigidBodies[i] == rb)
+        {
+            m_rigidBodies.erase(m_rigidBodies.begin() + i);
+            return;
+        }
+    }
+}
+
+void Simulation::run()
+{
+    for (int i = 0; i < m_iteration_cap; i++)
+    {
+        step();
+        m_iterations++;
+    }
+}
+
+void Simulation::step() {}
+
+void Simulation::handleCollisions()
+{
+    for (int rb_a = 0; rb_a < m_rigidBodies.size(); rb_a++)
+    {
+        for (int rb_b = 0; rb_b < m_rigidBodies.size(); rb_b++)
+        {
+            if (checkCollision(m_rigidBodies[rb_a], m_rigidBodies[rb_b]))
+                collide(m_rigidBodies[rb_a], m_rigidBodies[rb_b]);
+        }
+    }
+}
+
+bool Simulation::checkCollision(RigidBody *rb_a, RigidBody *rb_b)
+{
+    sf::Vector2f diff = rb_a->getPosition() - rb_b->getPosition();
+    if (diff.x * diff.x + diff.y * diff.y > rb_a->getBoundingRadius() + rb_b->getBoundingRadius())
+        return false;
+    if (rb_a->getColliderType() == RigidBody::ColliderType::CIRCLE &&
+        rb_b->getColliderType() == RigidBody::ColliderType::CIRCLE)
+        return true;
+    if (rb_a->getColliderType() == RigidBody::ColliderType::BOX)
+    {
+    }
+}
+
+void Simulation::collide(RigidBody *rb_a, RigidBody *rb_b) {}
+
+void Simulation::integrate(RigidBody *rb) {}
--- src/Physics/Simulation.h
+++ src/Physics/Simulation.h
@@ -7,15 +7,15 @@ class Simulation
 {
   public:
     Simulation(float time_step = 0.1, unsigned int iterations = 0, Display *diaplay = nullptr);
-    void initialize();
 
-    void addRigidBody(RigidBody &rb);
-    void removeRigidBody(RigidBody &rb);
+    void addRigidBody(RigidBody *rb) { m_rigidBodies.push_back(rb); }
+    void removeRigidBody(RigidBody *rb);
 
-    void run();
-
+    void setTimestep(float timestep) { m_time_step = timestep; }
     void addDisplay(Display *display) { m_display = display; }
     void iterationCap(unsigned int cap) { m_iteration_cap = cap; }
+
+    void run();
 
   private:
     sf::Clock m_clock;
@@ -26,5 +26,12 @@ class Simulation
     unsigned int m_iteration_cap;
     float m_elapsedTime;
 
-    void integrate(RigidBody &rb);
+    std::vector<RigidBody *> m_rigidBodies;
+
+    void step();
+    void handleCollisions();
+
+    bool checkCollision(RigidBody *rb_a, RigidBody *rb_b);
+    void collide(RigidBody *rb_a, RigidBody *rb_b);
+    void integrate(RigidBody *rb);
 };
